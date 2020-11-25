00b49f5d0a43d9137a9c6d90403894d8371a5a80
lucascf2901 <lucasfortes2901@gmail.com>
2020-11-25 17:46:47 +0000

    This is the source file with the method for multiplication

---
+++ LucasMultiplication.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca2tsktim;
+
+/**
+ *
+ * @author lucas
+ */
+public class LucasMultiplication {
+    
+      public static float Multiplication(float x, float y)
+    {
+        return x * y;
+    }
+    
+}
